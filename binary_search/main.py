88bf771b8a3af3bc8da4f1d97561a8017b47a3fa
Fabio Santos <fabiojunior96@outlook.com>
2024-12-28 18:18:04 -0300

    adicionando código para pesquisa binária

---
+++ binary_search/main.py
@@ -0,0 +1,23 @@
+import time 
+
+def binary_search (arr, item):
+    down = 0
+    high = len(arr) - 1
+    while down <= high:
+        middle = (down + high) // 2 # two slashes considers the integer part of division
+        guess = arr[middle]
+        if guess == item:
+            return middle
+        if guess > item:
+            high = middle - 1
+        else:
+            down = middle + 1
+
+my_list = [1, 3, 4, 5, 6 , 7, 8, 9, 10, 11, 12, 14, 16, 40, 55, 65, 66, 67, 68, 69]
+
+
+st = time.time()
+print(binary_search(my_list, 14))
+et = time.time()
+elepesed_time = (et - st) * 1000 # to calculate a execution time in milliseconds
+print('Execution time:', elepesed_time, 'milliseconds')
